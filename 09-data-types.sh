d16f7cc7cf746aace2ca2784c1194154f9ec1ec4
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-16 19:43:59 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# everything in shell is considered as string
 NUMBER1=100
 NUMBER2=200
 NAME=Devops
@@ -8,6 +9,9 @@ SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is: ${SUM}"
 
+# size=4, max index = 3
+
 LEADERS=("Modi" "Putin" "Trudo" "Trump")
 
 echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADER[0]}"
