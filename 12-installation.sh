21be0eaa45b18ec126d3b4807c538ca9db492e4b
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-17 19:15:46 +0530

    installing

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
      echo "ERROR:: Please run this script with root Privelege"
+     exit 1
 fi
 
 
@@ -11,5 +12,6 @@ dnf install mysql -y
 
 if [ $? -ne 0 ]; then
      echo "ERROR:: Installaing MYSQL is Failure"
+     exit 1
 else
     echo "Installing MYSQL is SUCCESS"
