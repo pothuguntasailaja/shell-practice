c03747b74d894a1d5f6123c2db53c156802f9c41
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-17 17:18:34 +0530

    conditions

---
+++ 11-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter the number:"
+read NUMBER
+
+if [ $(($NUMBER % 2)) -eq 0]; then
+     echo "Given number $NUMBER is a even number"
+else
+       echo "Given number $NUMBER is a odd number"
+fi         
