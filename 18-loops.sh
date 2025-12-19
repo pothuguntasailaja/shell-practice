883ac0509e66d6bf862972fdf49b9bf8a6798297
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-19 13:27:56 +0530

    loops

---
+++ 18-loops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root Privelege"
+     exit 1 # failure is other than 0
+fi
+
+VALIDATE (){
+    if [ $1 -ne 0 ]; then
+         echo -e " Installaing $2 .... $R Failure $N" | tee -a $LOG_FILE
+         exit 1
+    else
+         echo -e "Installing $2 .... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+# $@ => All Variables passed to the script
+
+for package  in  $@
+do 
+   echo "package is: $package"
+done   
+
