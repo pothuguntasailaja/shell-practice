3027fc1ec45671266864f26c051ea9ae0a5863f5
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-24 11:19:57 +0530

    hello

--- 19.sh
+++ 19.sh
@@ -8,3 +8,12 @@ echo "print the value of $person1"
 
 person2=$1
 echo "print the value of $person2"
+
+
+person3=ram
+person4=sita
+
+echo "$person3:: hi, $person4"
+echo "$person4:: hello, $person3"
+echo "$person3:: which course u are learning $person4"
+echo "$person4:: iam learning shell $person3"
