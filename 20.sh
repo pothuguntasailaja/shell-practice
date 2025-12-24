847e765471c7e61a1fb9451dbd0dc55f06e24ab9
pothugunta sailaja <pothuguntasailaja@gmail.com>
2025-12-24 17:40:31 +0530

    add

--- 20.sh
+++ 20.sh
@@ -8,12 +8,13 @@ Y="\e[33m"
 
 if [ $USERID -ne 0 ]; then
     echo "please run this script by root privilege"
+    exit 1
 fi    
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then 
     echo "not installed .... $R FAILURE $N"
+    exit 1
 else 
     echo "already installed ... $G SUCESS $N"    
-    
